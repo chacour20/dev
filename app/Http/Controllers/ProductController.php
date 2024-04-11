4d3324809d8059b3000b8e3be08ab527983695a7
ADAM  Chacour <adamchacour118@gmail.com>
2024-04-11 13:50:16 +0100

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+
+    public function index()
+    {
+        try
+        {
+            
+            $product = Product::latest()->paginate(10);
+                return view('templates.product.new', compact('product'))
+                    ->with('i', (request()->input('page', 1) - 1) * 5);
+        }
+        catch(\Illuminate\Database\QueryException $ex)
+        {
+            $error= "Une erreur inattendue s'est produite." ;
+            \Log::error($ex->getMessage());
+            return back()->with('error', $error)->withInput();
+        }
+    }
+
+    
+}
